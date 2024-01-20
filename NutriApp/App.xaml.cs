97a3f367d71df257083c45a32a3fba4a421236e0
ricardomasuda <m-ricardo1611@hotmail.com>
2024-01-19 22:51:07 -0300

    melhoria

--- NutriApp/App.xaml.cs
+++ NutriApp/App.xaml.cs
@@ -7,6 +7,7 @@ public partial class App : Application
     public static NavigationPage NavPage { get; set; }
     public App()
     {
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mjk2MjY4MkAzMjMzMmUzMDJlMzBFMnRySklPRjR4cGlWZ0U4WDFRR3NGOWkwUzF1VEZVd0tHRm1oUkwyUU5NPQ==");
         InitializeComponent();
 
         NavPage = new NavigationPage(new MainPage());
