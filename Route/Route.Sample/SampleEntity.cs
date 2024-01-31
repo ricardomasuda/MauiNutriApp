3e8a06f48b7f52125b80f61e602e7c0e4bf99ba6
lyncolnmauricio <lyncolnms@gmail.com>
2024-01-31 00:50:07 -0300

    feature: create project to route

---
+++ Route/Route.Sample/SampleEntity.cs
@@ -0,0 +1,11 @@
+using Generators;
+
+namespace Route.Sample;
+
+// This code will not compile until you build the project with the Source Generators
+
+[Report]
+public partial class SampleEntity {
+    public int Id { get; } = 42;
+    public string? Name { get; } = "Sample";
+}
