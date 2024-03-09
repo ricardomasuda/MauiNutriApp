8927e889bfd33d95cf3e32c3d01bc9f766d454f2
ricardomasuda <m-ricardo1611@hotmail.com>
2024-03-09 00:19:02 -0300

    adicionando navegacao

--- NutriApp/AppNutri/View/MainPage/MainPageViewModel.cs
+++ NutriApp/AppNutri/View/MainPage/MainPageViewModel.cs
@@ -1,3 +1,4 @@
+using MauiLib1.Navigation;
 using NutriApp.AppNutri.Componente;
 using NutriApp.AppNutri.View.About;
 using NutriApp.AppNutri.View.Evaluation.List;
@@ -10,18 +11,17 @@ public class MainPageViewModel : BaseViewModel
 {
     private string _version;
     public string Version { get => _version; set { _version = value; OnPropertyChanged("Version"); } }
-    public Command EvaluationCommand { get; set; } = new( () => Shell.Current.GoToAsync(nameof(EvaluationListPage)));
+    public Command EvaluationCommand { get; set; } =  new ( () =>  App.NavPage.GoToAsync(nameof(EvaluationListPage)));
     public Command AnthropometricEvaluationCommand { get; set; }
-    public Command ListFoodCommand { get; set; } = new ( () => Shell.Current.GoToAsync(nameof(FoodListPage)));
+    public Command ListFoodCommand { get; set; } = new ( () => App.NavPage.GoToAsync(nameof(FoodListPage)));
     public Command FoodPlanCommand { get; set; }
-    public Command SuggestionCommand { get; set; } = new( () => Shell.Current.GoToAsync(nameof(SuggestionPage)));
-    public Command AboutAppCommand { get; set; } = new( () => Shell.Current.GoToAsync(nameof(AboutPage)));
+    public Command SuggestionCommand { get; set; } = new( () => App.NavPage.GoToAsync(nameof(SuggestionPage)));
+    public Command AboutAppCommand { get; set; } = new( () => App.NavPage.GoToAsync(nameof(AboutPage)));
     // FoodPlanCommand = new Command( () => Navigation.PushPageAsync(new FoodPlanList()));
     // AnthropometricEvaluationCommand = new Command( () => Navigation.PushPageAsync(new AnthropometricEvaluationPage()));
-    //FillPage();
-
-    // private void FillPage()
-    // {
-    //     Version = App.Version;
-    // }
+ 
+    public MainPageViewModel() 
+    {
+        App.NavPage = Application.Current.MainPage.Handler.MauiContext.Services.GetService<INavigationService>();
+    }
 }
