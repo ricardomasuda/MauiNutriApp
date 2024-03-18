7d78c6f340fe914d6c607cd011c0535bd32a8aac
lyncolnmauricio <lyncolnms@gmail.com>
2024-03-18 12:00:57 -0300

    fix merge

--- NutriApp/Views/Food/List/FoodListPage.xaml.cs
+++ NutriApp/Views/Food/List/FoodListPage.xaml.cs
@@ -10,16 +10,10 @@ public partial class FoodListPage {
         BindingContext = _viewModel;
     }
 
-
     // private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
     // {
     //     ListFood.ItemsSource = string.IsNullOrEmpty(e.NewTextValue) ? FoodViewModel.ListFood : FoodViewModel.ListFood.Where(x => x.Nome.ToUpper().Contains(e.NewTextValue.ToUpper()));
     // } 
-
-    protected override bool OnBackButtonPressed() {
-        App.NavPage.PopAsync();
-        return base.OnBackButtonPressed();
-    }
 
     private void OnSizeChanged(object sender, EventArgs e) {
         if (sender is SearchTitleView searchTitleView) _viewModel.SearchTitleHeight = searchTitleView.Height;
