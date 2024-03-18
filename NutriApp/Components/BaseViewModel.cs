93a830dc5cf1e05af241e2f2e5f0c9b2ae505c37
ricardomasuda <m-ricardo1611@hotmail.com>
2024-03-18 00:08:22 -0300

    arrumando o base viewmodel

--- NutriApp/Components/BaseViewModel.cs
+++ NutriApp/Components/BaseViewModel.cs
@@ -1,9 +1,11 @@
 using System.ComponentModel;
-using NutriApp.AppNutri.Componente;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace NutriApp.Components;
 
-public partial class BaseViewModel : IBaseViewModel, INotifyPropertyChanged
+public partial class BaseViewModel : ObservableObject 
 {
     // protected void ErrorToaster(string message = "Error!", int durationInSeconds = 3, bool showInBottom = false)
     // {
@@ -17,11 +19,15 @@ public partial class BaseViewModel : IBaseViewModel, INotifyPropertyChanged
     //     _feedBackService.Toaster(message, durationInSeconds, showInBottom, EnumToasterType.Warning);
     // }
     //
-    // protected void InfoToaster(string message, int durationInSeconds = 3, bool showInBottom = false)
-    // {
-    //     UserDialogs.Instance.HideLoading();
-    //     _feedBackService.Toaster(message, durationInSeconds, showInBottom, EnumToasterType.Info);
-    // }
+    protected async void InfoToaster(string message, ToastDuration toastDuration)
+    {
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        double fontSize = 14;
+        var toast = Toast.Make(message, toastDuration, fontSize);
+
+        await toast.Show(cancellationTokenSource.Token);
+    }
     //
     // protected void SuccessToaster(string message = "Successo!", int durationInSeconds = 3, bool showInBottom = false)
     // {
