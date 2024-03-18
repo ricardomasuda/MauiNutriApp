3c191ecabf7def0a4a28512acae7e4d7b96bd092
lyncolnmauricio <lyncolnms@gmail.com>
2024-03-18 11:33:12 -0300

    feature: add GlobalUsings

---
+++ NutriApp/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using NutriApp.Models;
+global using NutriApp.Views;
+global using NutriApp.Database;
+global using NutriApp.Components;
+global using NutriApp.Services;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
+global using System.Text.Json;
