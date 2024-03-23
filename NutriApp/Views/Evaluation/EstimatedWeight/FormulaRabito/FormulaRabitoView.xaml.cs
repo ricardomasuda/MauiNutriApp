903b5db0a290ad1857ce95ca4b28167a6510a380
ricardomasuda <m-ricardo1611@hotmail.com>
2024-03-22 23:26:23 -0300

    adicionando formula de rabito

---
+++ NutriApp/Views/Evaluation/EstimatedWeight/FormulaRabito/FormulaRabitoView.xaml.cs
@@ -0,0 +1,9 @@
+namespace NutriApp.Views.Evaluation.EstimatedWeight.FormulaRabito;
+
+public partial class FormulaRabitoView : ContentView
+{
+    public FormulaRabitoView()
+    {
+        InitializeComponent();
+    }
+}
