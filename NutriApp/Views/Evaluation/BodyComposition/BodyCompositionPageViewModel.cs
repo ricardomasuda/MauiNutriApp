b47c6c9b478b61015eabc201dd2cfe1c3f0958ce
ricardomasuda <m-ricardo1611@hotmail.com>
2024-03-23 14:11:30 -0300

    adicionando MVVM na Composição corporal

--- NutriApp/Views/Evaluation/BodyComposition/BodyCompositionPageViewModel.cs
+++ NutriApp/Views/Evaluation/BodyComposition/BodyCompositionPageViewModel.cs
@@ -1,173 +1,53 @@
-using System.Collections.ObjectModel;
-using NutriApp.Components;
-using NutriApp.Models;
+using NutriApp.AppUtilities;
 using NutriApp.Services.AnthropometricEvaluation;
 
 namespace NutriApp.Views.Evaluation.BodyComposition;
 
-public class BodyCompositionPageViewModel : BaseViewModel
+public partial class BodyCompositionPageViewModel : BaseViewModel
 {
+    [ObservableProperty]
     private string _height;
-    public string Height
-    {
-        get => _height;
-        set
-        {
-            _height = value;
-            OnPropertyChanged("Height");
-        }
-    }
-
+    [ObservableProperty]
     private string _wrist;
-    public string Wrist
-    {
-        get => _wrist;
-        set
-        {
-            _wrist = value;
-            OnPropertyChanged("Wrist");
-        }
-    }
-
+    [ObservableProperty]
     private string _femur;
-    public string Femur
-    {
-        get => _femur;
-        set
-        {
-            _femur = value;
-            OnPropertyChanged("Femur");
-        }
-    }
-
+    [ObservableProperty]
     private string _weight;
-    public string Weight
-    {
-        get => _weight;
-        set
-        {
-            _weight = value;
-            OnPropertyChanged("Weight");
-        }
-    }
-
+    [ObservableProperty]
     private bool _hasErrorHeight;
-    public bool HasErrorHeight
-    {
-        get => _hasErrorHeight;
-        set
-        {
-            _hasErrorHeight = value;
-            OnPropertyChanged("HasErrorHeight");
-        }
-    }
-
+    [ObservableProperty]
     private bool _hasErrorWrist;
-    public bool HasErrorWrist
-    {
-        get => _hasErrorWrist;
-        set
-        {
-            _hasErrorWrist = value;
-            OnPropertyChanged("HasErrorWrist");
-        }
-    }
-
+    [ObservableProperty]
     private bool _hasErrorFemur;
-    public bool HasErrorFemur
-    {
-        get => _hasErrorFemur;
-        set
-        {
-            _hasErrorFemur = value;
-            OnPropertyChanged("HasErrorFemur");
-        }
-    }
-
+    [ObservableProperty]
     private bool _hasErrorWeight;
-    public bool HasErrorWeight
-    {
-        get => _hasErrorWeight;
-        set
-        {
-            _hasErrorWeight = value;
-            OnPropertyChanged("HasErrorWeight");
-        }
-    }
-
+    [ObservableProperty]
     private bool _hasErrorGender;
-    public bool HasErrorGender
-    {
-        get => _hasErrorGender;
-        set
-        {
-            _hasErrorGender = value;
-            OnPropertyChanged("HasErrorGender");
-        }
-    }
-
+    [ObservableProperty]
     private double _boneMassResult;
-    public double BoneMassResult
-    {
-        get => _boneMassResult;
-        set
-        {
-            _boneMassResult = value;
-            OnPropertyChanged("BoneMassResult");
-        }
-    }
-
+    [ObservableProperty]
     private double _residualWeightResult;
-    public double ResidualWeightResult
-    {
-        get => _residualWeightResult;
-        set
-        {
-            _residualWeightResult = value;
-            OnPropertyChanged("ResidualWeightResult");
-        }
-    }
-
+    [ObservableProperty]
     private Item _genderType;
-    public Item GenderType
-    {
-        get => _genderType;
-        set
-        {
-            _genderType = value;
-            OnPropertyChanged("GenderType");
-        }
-    }
-
+    [ObservableProperty]
     private bool _canDisplayResult;
-    public bool CanDisplayResult
-    {
-        get => _canDisplayResult;
-        set
-        {
-            _canDisplayResult = value;
-            OnPropertyChanged("CanDisplayResult");
-        }
-    }
-
     public ObservableCollection<Item> ListGender { get; set; }
-    public Command CalculateCommand { get; set; }
 
     public BodyCompositionPageViewModel()
     {
-        CalculateCommand = new Command(Calculate);
         ListGender = new ObservableCollection<Item>
         {
             new() { Id = 0, Nome = "Homem" },
             new() { Id = 1, Nome = "Mulher" }
         };
     }
-
+    
+    [RelayCommand]
     private void Calculate()
     {
         if (ValidateData())
         {
-            BoneMassResult = BodyCompositionService.CalculateBoneMass(Convert.ToDouble(Height), Convert.ToDouble(Wrist),
+            BoneMassResult = BodyCompositionService.CalculateBoneMass( Utils.ParseToDoubleWithCommaSeparator(Height), Convert.ToDouble(Wrist),
                 Convert.ToDouble(Femur));
             ResidualWeightResult = BodyCompositionService.CalculateResidualWeight(Convert.ToDouble(Weight),
                 GenderUtils.ConverterGender(GenderType.Nome));
