6626a83e8e5055f6eca8fc9e4a4afb8d851cfb07
lyncolnmauricio <lyncolnms@gmail.com>
2024-03-28 00:51:34 -0300

    feature: ajustando para novo mvvm

--- NutriApp/Views/Food/Detail/FoodDetailPage.xaml.cs
+++ NutriApp/Views/Food/Detail/FoodDetailPage.xaml.cs
@@ -1,19 +1,9 @@
-using NutriApp.Models;
-
 namespace NutriApp.Views.Food.Detail;
 
-public partial class FoodDetailPage : ContentPage
-{
-    private FoodDetailViewModel FoodDetailViewModel { get; set; }
-    public FoodDetailPage(FoodModel food)
-    {
+public partial class FoodDetailPage {
+    public FoodDetailPage() {
         InitializeComponent();
-        //BindingContext = new FoodDetailViewModel(this);
-    }
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-      
+        BindingContext = new FoodDetailViewModel();
     }
 
     // private void InputView_OnTextChanged(object sender, TextChangedEventArgs e)
--- NutriApp/Views/Food/Detail/FoodDetailViewModel.cs
+++ NutriApp/Views/Food/Detail/FoodDetailViewModel.cs
@@ -1,116 +1,33 @@
+using NutriApp.Views.Food.Detail.InfoPopup;
+
 namespace NutriApp.Views.Food.Detail;
 
-[QueryProperty(nameof(Food), "food")]
-public class FoodDetailViewModel : BaseViewModel, IQueryAttributable
-{
-    private double _measure;
-    public double Measure
-    {
-        get => _measure;
-        set
-        {
-            _measure = value;
-            OnPropertyChanged();
-        }
-    }
-
-    private string _carbohydrates;
-
-    public string Carbohydrates
-    {
-        get => _carbohydrates;
-        set
-        {
-            _carbohydrates = value;
-            OnPropertyChanged();
-        }
-    }
-
-    private string _proteins;
-
-    public string Proteins
-    {
-        get => _proteins;
-        set
-        {
-            _proteins = value;
-            OnPropertyChanged();
-        }
-    }
-
-    private string _lipids;
-
-    public string Lipids
-    {
-        get => _lipids;
-        set
-        {
-            _lipids = value;
-            OnPropertyChanged();
-        }
-    }
-
-    private string _sodium;
-    public string Sodium
-    {
-        get => _sodium;
-        set
-        {
-            _sodium = value;
-            OnPropertyChanged();
-        }
-    }
-
-    private string _dietaryFiber;
-
-    public string DietaryFiber
-    {
-        get => _dietaryFiber;
-        set
-        {
-            _dietaryFiber = value;
-            OnPropertyChanged();
-        }
-    }
-
-    private string _calorificValue;
-    public string CalorificValue
-    {
-        get => _calorificValue;
-        set
-        {
-            _calorificValue = value;
-            OnPropertyChanged();
-        }
-    }
-    
-    private FoodModel _food;
-    public FoodModel Food
-    {
-        get => _food;
-        set
-        {
-            _food = value;
-            OnPropertyChanged();
-        }
-    }
+[QueryProperty(nameof(SelectedFood), "selectedFood")]
+public partial class FoodDetailViewModel : BaseViewModel, IQueryAttributable {
+    [ObservableProperty] private double _measure;
+    [ObservableProperty] private string _carbohydrates;
+    [ObservableProperty] private string _proteins;
+    [ObservableProperty] private string _lipids;
+    [ObservableProperty] private string _sodium;
+    [ObservableProperty] private string _dietaryFiber;
+    [ObservableProperty] private string _calorificValue;
+    [ObservableProperty] private FoodModel _selectedFood;
 
     public string Source { get; set; }
-    public Command InfoCommand { get; set; }
-    public Command GoReportCommand { get; set; }
 
-    public FoodDetailViewModel()
-    {
-        //InfoCommand = new Command(() => foodDetailPage.ShowPopup(new InformationFoodPopup()));
-        GoReportCommand = new Command(GoReport);
+    public FoodDetailViewModel() {
         Fill();
     }
 
-    public async void ChangeMeasure(bool isZero)
-    {
-        if (Food == null) return;
+    [RelayCommand]
+    private void ShowFoodInformation() {
+        App.NavPage.ShowPopup(new InformationFoodPopup());
+    }
+
+    private async void ChangeMeasure(bool isZero) {
         Measure = isZero ? 0 : Measure;
-        var food = await FoodService.ChangeUnitMeasure(Food.Id, Measure);
+        FoodModel food = await FoodService.ChangeUnitMeasure(SelectedFood.Id, Measure);
+
         Carbohydrates = food.Carboidratos;
         Proteins = food.Proteinas;
         Lipids = food.Lipidios;
@@ -119,26 +36,22 @@ public class FoodDetailViewModel : BaseViewModel, IQueryAttributable
         CalorificValue = food.ValorCalorico;
     }
 
-    private void Fill()
-    {
-        if (Food != null)
-            Measure = Convert.ToInt32(Food.Medida) != 0 ? Convert.ToDouble(Food.Medida) : 100;
-        else
-            Measure = 100;
-        Source = Food != null ? FoodReference.GetReference(Food.Fonte) : null;
-        
+    private void Fill() {
+        Measure = Convert.ToInt32(SelectedFood.Medida) is not 0 ? Convert.ToDouble(SelectedFood.Medida) : 100;
+        Source = FoodReference.GetReference(SelectedFood.Fonte);
     }
 
-    private async void GoReport()
-    {
-        // var foodReport = await FoodService.ChangeUnitMeasure(Food.Id, Measure);
-        // List<FoodModel> listFood = new List<FoodModel> { foodReport };
-        // await App.NavPage.PushAsync(new ReportPage(listFood));
+    [RelayCommand]
+    private async void GoReportAsync() {
+        FoodModel foodReport = await FoodService.ChangeUnitMeasure(SelectedFood.Id, Measure);
+        List<FoodModel> listFood = [foodReport];
+
+        // await App.NavPage.GoToAsync(new ReportPage(listFood));
     }
-    public void ApplyQueryAttributes(IDictionary<string, object> query)
-    {
-        Food = query["food"] as FoodModel;
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query) {
+        SelectedFood = (FoodModel)query["selectedFood"];
+
         ChangeMeasure(false);
     }
-    
 }
