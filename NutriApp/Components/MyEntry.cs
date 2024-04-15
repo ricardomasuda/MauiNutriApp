c7704ee76b266b2f35a7bbab625d098a17e1e7be
lyncolnmauricio <lyncolnms@gmail.com>
2024-04-15 09:13:45 -0300

    feature: creating floating entry component

--- NutriApp/Components/MyEntry.cs
+++ NutriApp/Components/MyEntry.cs
@@ -1,14 +1,11 @@
 namespace NutriApp.Components;
 
-public class MyEntry : Entry
-{
-    public static readonly BindableProperty CornerRadiusProperty =  
-        BindableProperty.Create(nameof(CornerRadius),  
-            typeof(double),typeof(MyEntry));  
-    // Gets or sets CornerRadius value  
-    public double CornerRadius  
-    {  
-        get =>(double)GetValue(CornerRadiusProperty);   
-        set => SetValue(CornerRadiusProperty, value);   
+public class MyEntry : Microsoft.Maui.Controls.Entry {
+    public static readonly BindableProperty CornerRadiusProperty =
+        BindableProperty.Create(nameof(CornerRadius), typeof(double), typeof(MyEntry));
+
+    public double CornerRadius {
+        get => (double)GetValue(CornerRadiusProperty);
+        init => SetValue(CornerRadiusProperty, value);
     }
 }
