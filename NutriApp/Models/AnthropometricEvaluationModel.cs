781683181e997310f782756ff26921884fd8fcda
ricardomasuda <m-ricardo1611@hotmail.com>
2024-04-20 22:39:41 -0300

    adicionando model

---
+++ NutriApp/Models/AnthropometricEvaluationModel.cs
@@ -0,0 +1,15 @@
+namespace NutriApp.Models;
+
+public class AnthropometricEvaluationModel
+{
+    public double Abdominal { get; set; }
+    public double Biceps { get; set; }
+    public double Chest { get; set; }
+    public double MedialAxilla { get; set; }
+    public double MiddleAxillary { get; set; }
+    public double LateralCalf { get; set; }
+    public double Triceps { get; set; }
+    public double Thigh { get; set; }
+    public double SubScapular { get; set; }
+    public double SupraIliac { get; set; }
+}
