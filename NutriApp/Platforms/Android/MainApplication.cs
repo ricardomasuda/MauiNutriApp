62b0aef6fc78f5ce8c29ae4c8a5bc502b6275664
ricardomasuda <m-ricardo1611@hotmail.com>
2024-07-09 17:30:06 -0300

    adicionando novas configurações

--- NutriApp/Platforms/Android/MainApplication.cs
+++ NutriApp/Platforms/Android/MainApplication.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using Android.Content.Res;
 using Android.Runtime;
 
 namespace NutriApp;
@@ -6,9 +7,26 @@ namespace NutriApp;
 [Application]
 public class MainApplication : MauiApplication
 {
-    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-        : base(handle, ownership)
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership): base(handle, ownership)
     {
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
+        {
+            if (view is Entry)
+            {
+                handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+
+                handler.PlatformView.SetHintTextColor(ColorStateList.ValueOf(Android.Graphics.Color.Red));
+            }
+        });
+        
+        Microsoft.Maui.Handlers.PickerHandler.Mapper.AppendToMapping(nameof(Picker), (handler, view) =>
+        {
+            if (view is Picker)
+            {
+                handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+                handler.PlatformView.SetHintTextColor(ColorStateList.ValueOf(Android.Graphics.Color.Red));
+            }
+        });
     }
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
