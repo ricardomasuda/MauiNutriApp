a624fd6cab0ee3514c50bedd2385676486e13b73
ricardomasuda <m-ricardo1611@hotmail.com>
2024-07-09 17:35:06 -0300

    removendo syncfusion

--- NutriApp/MauiProgram.cs
+++ NutriApp/MauiProgram.cs
@@ -1,7 +1,8 @@
 ﻿using CommunityToolkit.Maui;
+using MauiLib1;
+using MauiLib1.Components.Entries;
 using MauiLib1.Navigation;
 using Microsoft.Extensions.Logging;
-using Syncfusion.Maui.Core.Hosting;
 
 namespace NutriApp;
 
@@ -15,7 +16,6 @@ public static class MauiProgram
         
         builder
             .UseMauiApp<App>()
-            .ConfigureSyncfusionCore()
             .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
@@ -23,7 +23,14 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("Font Awesome 6 Free-Regular-400.otf", "FontAwesomeRegular");
                 fonts.AddFont("Font Awesome 6 Free-Solid-900.otf", "FontAwesomeBold");
+            }).ConfigureMauiHandlers(handlers =>
+            {
+#if IOS
+                handlers.AddHandler(typeof(BorderlessEntry), typeof(BorderlessEntryHandler));
+#endif
             });
+            
+            ;
 
 #if DEBUG
         builder.Logging.AddDebug();
